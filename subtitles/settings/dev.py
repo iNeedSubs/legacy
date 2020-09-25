1e012a6e62e5d11eca0e22e5918fe07854ad2b54
Spimy <officialspyme@gmail.com>
2020-09-25 21:04:55 +0400

    Project Layout Setup

---
+++ subtitles/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
