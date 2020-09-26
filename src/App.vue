fe39cda2f4b3a0c193b96cb0985dd64228a38281
Eray Chumak <eray_chumak@protonmail.com>
2020-09-26 22:50:00 +0100

    add Source Sans Pro font

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -10,16 +15,31 @@
   font-family: Rubik;
   src: url("assets/fonts/rubik_bold.ttf");
 }
-}
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: Source Sans Pro SemiBold;
+  src: url("assets/fonts/source_sans_pro_semi_bold.ttf");
+}
+
+@font-face {
+  font-family: Source Sans Pro Regular;
+  src: url("assets/fonts/source_sans_pro_regular.ttf");
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #app {
+  height: 100%;
+  width: 100%;
+}
+
+body {
+  font-family: Source Sans Pro Regular;
+  background: #222831;
+  color: #fff;
 }
 </style>
