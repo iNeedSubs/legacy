f36cbba02073319cb1265b48a826067ff3baf9c9
Eray Chumak <eray_chumak@protonmail.com>
2020-09-26 22:50:30 +0100

    add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>Subtitles</h1>
+        <Nav/>
+    </header>
+</template>
+
+<script>
+import Nav from './Nav'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background: #d65a31;
+  padding: 30px;
+  text-align: center;
+}
+
+h1 {
+  font-size: 4em;
+  margin-bottom: .5em;
+  font-family: Rubik;
+}
+</style>
