4fa7ea9da57ed08670857e1a1412baed0e4d9c90
Eray Chumak <eray_chumak@protonmail.com>
2020-09-26 22:47:51 +0100

    add fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSearch)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
