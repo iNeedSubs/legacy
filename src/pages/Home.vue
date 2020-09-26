2adb8c0f9f171b1f1ac45f0f22791a05df0885c9
Eray Chumak <eray_chumak@protonmail.com>
2020-09-26 22:51:38 +0100

    add Home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Header/>
+    <main>
+      <p class="desc">Download subtitles for Movies, TV Shows and Anime in your preferred language.</p>
+      <Search/>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+main {
+  margin-top: 1em;
+  padding: 30px;
+}
+
+.desc {
+  margin-bottom: 2em;
+  font-size: 18px;
+  text-align: center;
+}
+</style>
+
+<script>
+import Header from '../components/Header'
+import Search from '../components/Search'
+
+export default {
+  components: {
+    Header,
+    Search
+  }
+}
+</script>
