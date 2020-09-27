1c801d0a10e92ec2a09895e429bc40fd6efead7c
Spimy <officialspyme@gmail.com>
2020-09-27 17:18:58 +0400

    Update search endpoint to return only wanted results

--- apps/middleman_api/views.py
+++ apps/middleman_api/views.py
@@ -1,9 +1,13 @@
 import requests
+from typing import List
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 
 EXTERNAL_BASE_URL = 'https://rest.opensubtitles.org'
+HEADERS = {
+    'User-Agent': 'TemporaryUserAgent'
+}
 
 
 class SearchEndpoint(GenericAPIView):
@@ -12,16 +16,35 @@ class SearchEndpoint(GenericAPIView):
         return Response(self.get_queryset())
 
     def get_queryset(self):
-        query = self.request.query_params.get('query', None)
-        language = self.request.query_params.get('language', None)
+        query: str or None = self.request.query_params.get('query', None)
+        language: str = self.request.query_params.get('language', 'eng')
+        query_type: str = self.request.query_params.get('type', 'movie')
 
         if query is None:
             return {'err': 'Provide a search query.'}
 
         external_api = f'{EXTERNAL_BASE_URL}/search/query-{query}/sublanguageid-{language}'
-        headers = {
-            'User-Agent': 'TemporaryUserAgent'
-        }
+        response = requests.get(external_api, headers=HEADERS)
+        results: List[dict] = response.json()
 
-        response = requests.get(external_api, headers=headers)
-        return response.json()
+        final: List[dict] = []
+
+        for result in results:
+            if result.get('MovieKind') not in ['tv series', 'episode']:
+                if result.get('MovieKind') == query_type.lower():
+                    current = self.set_result(result)
+
+            current = self.set_result(result)
+
+            final.append(current)
+
+        return final
+
+    def set_result(self, result) -> dict:
+        current = {
+            'title': result.get('MovieReleaseName'),
+            'name': result.get('SubFileName'),
+            'language': result.get('SubLanguageID'),
+            'downloadUrl': result.get('SubDownloadLink')
+        }
+        return current
