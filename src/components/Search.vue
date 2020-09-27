be24b96a03c9e73351c923152ab99e1a73c19add
Eray Chumak <eray_chumak@protonmail.com>
2020-09-27 15:16:54 +0100

    add shadow to language selector

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -39,67 +39,67 @@ export default {
 
 <style scoped>
 .search {
-    background: #51555c;
-    border-radius: 5px;
+  background: #51555c;
+  border-radius: 5px;
 }
 
 .inputContainer {
-    display: flex;
-    border-radius: 5px;
+  display: flex;
+  border-radius: 5px;
 }
 
 input {
-    background: #51555c;
-    padding: 1em 0 1em 1em;
-    width: 100%;
-    min-width: 0;
-    border: none;
-    border-radius: 5px;
-    font-family: Source Sans Pro Regular;
-    font-size: 18px;
-    color: #fff;
-    outline: none;
+  background: #51555c;
+  padding: 1em 0 1em 1em;
+  width: 100%;
+  min-width: 0;
+  border: none;
+  border-radius: 5px;
+  font-family: Source Sans Pro Regular;
+  font-size: 18px;
+  color: #fff;
+  outline: none;
 }
 
 input::placeholder {
-    color: #fff;
+  color: #fff;
 }
 
 button {
-    border: none;
-    cursor: pointer;
-    outline: none;
+  border: none;
+  cursor: pointer;
+  outline: none;
 }
 
 .inputContainer button {
-    margin: 10px;
-    padding: 0 1em;
-    background: #d65a31;
-    border-radius: 5px;
-    color: #fff;
-    transition: background .1s ease-in-out;
+  margin: 10px;
+  padding: 0 1em;
+  background: #d65a31;
+  border-radius: 5px;
+  color: #fff;
+  transition: background .1s ease-in-out;
 }
 
 .inputContainer button:hover {
-    background: #de7b5a;
+  background: #de7b5a;
 }
 
 .filters {
-    border-radius: 5px;
-    background: #494d54;
-    display: grid;
-    grid-template-areas:
-        "movie tvShow anime"
-        "lang lang lang";
+  border-radius: 5px;
+  background: #494d54;
+  display: grid;
+  grid-template-areas:
+    "movie tvShow anime"
+    "lang lang lang";
 }
 
 .filters button {
-    background: #494d54;
-    padding: .5em 1em;
-    color: #fff;
-    font-family: Source Sans Pro SemiBold;
-    font-size: 18px;
-    transition: background .1s ease-in-out;
+  background: #494d54;
+  padding: .5em 1em;
+  color: #fff;
+  font-family: Source Sans Pro SemiBold;
+  font-size: 18px;
+  transition: background .1s ease-in-out;
 }
 
 .filters button:hover {
@@ -107,23 +107,24 @@ button {
 }
 
 .filters button.active {
-    background: #d65a31;
+  background: #d65a31;
 }
 
 .movie {
-    grid-area: movie;
+  grid-area: movie;
 }
 
 .tvShow {
-    grid-area: tvShow;
+  grid-area: tvShow;
 }
 
 .anime {
-    grid-area: anime;
+  grid-area: anime;
 }
 
 .lang {
-    grid-area: lang;
-    border-radius: 0 0 5px 5px;
+  box-shadow: 0px -1px 15px #22283155;
+  grid-area: lang;
+  border-radius: 0 0 5px 5px;
 }
 </style>
