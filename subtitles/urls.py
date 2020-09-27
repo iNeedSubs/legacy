c99eb82af6f80f02186603b8dd7bbc4855b192fe
Spimy <officialspyme@gmail.com>
2020-09-27 15:59:45 +0400

    Include middleman api url

--- subtitles/urls.py
+++ subtitles/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html'), name='app')
+    path('api/', include('apps.middleman_api.urls')),
+    url(r'^.*', TemplateView.as_view(template_name='index.html'), name='app')
 ]
 
 if settings.DEBUG:
