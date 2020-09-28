72267a1413795380bbbbea7ff0005452d1018348
Spimy <officialspyme@gmail.com>
2020-09-28 22:23:14 +0400

    Rename app name

--- apps/v1/search/urls.py
+++ apps/v1/search/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SearchEndpoint
 
 
-app_name = 'middleman_v1'
+app_name = 'search_v1'
 
 urlpatterns = [
     path('search/', SearchEndpoint.as_view(), name='search'),
