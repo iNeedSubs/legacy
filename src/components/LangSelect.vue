fe29bdae1af949105f445ca7ff3800d6ce5548ea
Eray Chumak <eray_chumak@protonmail.com>
2020-09-30 18:21:14 +0100

    Rotate drop down icon on options open/close

--- src/components/LangSelect.vue
+++ src/components/LangSelect.vue
@@ -1,15 +1,24 @@
 <template>
-  <button @click="toggleOptions" class="lang">{{name}} <fa icon="angle-down"/></button>
-  <div class="options" v-if="showOptions">
-    <p
-      v-for="(langName, langCode) in languages"
-      :key="langName"
-      :class="{active: code === langCode.toUpperCase()}"
-      @click="setLang(langCode)"
-    >
-      {{langName}}
-    </p>
-  </div>
+  <button @click="toggleOptions" class="lang">
+    {{name}}
+    <fa
+      class="downIcon"
+      :class="{active: showOptions}"
+      icon="angle-down"
+    />
+  </button>
+  <transition name="bounceIn">
+    <div class="options" v-if="showOptions">
+      <p
+        v-for="(langName, langCode) in languages"
+        :key="langName"
+        :class="{active: code.toUpperCase() === langCode.toUpperCase()}"
+        @click="setLang(langCode)"
+      >
+        {{langName}}
+      </p>
+    </div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -77,6 +86,7 @@ button {
 }
 
 .options {
+  margin: 0 auto;
   height: 150px;
   overflow-y: auto;
   white-space: nowrap;
@@ -86,6 +96,18 @@ button {
   position: absolute;
   background: #51555c;
   border-radius: 5px;
+
+  &::-webkit-scrollbar {
+    &-track {
+      border-radius: 0 5px 5px 0;
+      background: #333942;
+    }
+
+    &-thumb {
+      border-radius: 0 5px 5px 0;
+      background: #d65a31;
+    }
+  }
 }
 
 p {
@@ -96,7 +118,7 @@ p {
   align-items: center;
   width: 100%;
   height: 30px;
-  transition: background .1s ease-in-out;
+  transition: background .2s ease-in-out;
 
   &:hover {
     background: #6d707622;
@@ -104,6 +126,14 @@ p {
 
   &.active {
     background: #6d7076;
+  }
+}
+
+.downIcon {
+  transition: transform .4s cubic-bezier(0.2, 0.8, 0.3, 1.3);
+
+  &.active {
+    transform: rotate(-180deg);
   }
 }
 
