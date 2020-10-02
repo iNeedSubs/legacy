acfa71d459fce60b40eb6f6c432c91296187de5d
Eray Chumak <eray_chumak@protonmail.com>
2020-10-02 21:17:40 +0100

    Add movie and show routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,34 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import About from '../views/About.vue';
 import Contact from '../views/Contact.vue';
+import Show from '../views/Show.vue';
+import Movie from '../views/Movie.vue';
 import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
-    name: 'About',
+    name: 'about',
     component: About,
   },
   {
     path: '/contact',
-    name: 'Contact',
+    name: 'contact',
     component: Contact,
   },
   {
+    path: '/show/:id',
+    name: 'show',
+    component: Movie,
+  },
+  {
+    path: '/movie/:id',
+    name: 'movie',
+    component: Movie
+  },
+  {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
   }
 ];
