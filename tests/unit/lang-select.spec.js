7563e0c246707b6294a8d0cafc0c3a7ca9170978
Eray Chumak <eray_chumak@protonmail.com>
2020-10-02 19:42:50 +0100

    Add tests for lang-select button

--- tests/unit/lang-select.spec.js
+++ tests/unit/lang-select.spec.js
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils'
 import LangSelect from '../../src/components/LangSelect'
+import { LangCode, LangName } from '../../src/ts/languages'
 import { FontAwesomeIcon } from '../../src/plugins/fa'
 
 const defaults = {
@@ -10,18 +11,34 @@ const defaults = {
   }
 }
 
-describe('Language options menu', () => {
-  it('is hidden by default', () => {
-    const wrapper = mount(LangSelect, defaults)
-    expect(wrapper.find('.options').exists()).toBeFalsy()
-  })
-
-  it('appears when clicking on button', async () => {
-    const wrapper = mount(LangSelect, {
-      ...defaults,
+describe('Language Selection', () => {
+  describe('Button', () => {
+    it('shows correct language after selection', async () => {
+      const wrapper = mount(LangSelect, defaults)
+      await wrapper.vm.setLang(LangCode.RUSSIAN)
+      expect(wrapper.find('button.lang').text()).toBe(LangName.RUS)
     })
 
-    await wrapper.find('button.lang').trigger('click')
-    expect(wrapper.find('.options').exists()).toBeTruthy()
+    it('emits lang-update event once with the new lang code', async () => {
+      const wrapper = mount(LangSelect, defaults)
+      await wrapper.vm.setLang(LangCode.RUSSIAN)
+
+      expect(wrapper.emitted('update-lang')).toBeTruthy()
+      expect(wrapper.emitted('update-lang').length).toBe(1)
+      expect(wrapper.emitted('update-lang')[0]).toEqual(['rus'])
+    })
+  })
+
+  describe('Menu', () => {
+    it('is hidden by default', () => {
+      const wrapper = mount(LangSelect, defaults)
+      expect(wrapper.find('.options').exists()).toBeFalsy()
+    })
+
+    it('appears when clicking on button', async () => {
+      const wrapper = mount(LangSelect, defaults)
+      await wrapper.find('button.lang').trigger('click')
+      expect(wrapper.find('.options').exists()).toBeTruthy()
+    })
   })
 })
