9729e1e58e6b293445f125ae5a072e84e7c4fe22
Spimy <officialspyme@gmail.com>
2020-10-04 15:18:57 +0400

    Redirect admin url with no slash to actual admin url

--- subtitles/urls.py
+++ subtitles/urls.py
@@ -18,9 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
+    path('admin', RedirectView.as_view(url='admin/')),
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.v1.search.urls')),
     url(r'^.*', TemplateView.as_view(template_name='index.html'), name='app')
