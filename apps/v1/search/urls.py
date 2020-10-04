a93a981680d344a58623615cc3a9a498f36e7d1f
Spimy <officialspyme@gmail.com>
2020-10-04 15:27:18 +0400

    Remove access to endpoint with trailing slash

--- apps/v1/search/urls.py
+++ apps/v1/search/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from django.conf.urls import url
 from .views import Search, SearchMedia, SearchSubtitles
 
 
 app_name = 'search_v1'
 
 urlpatterns = [
-    url(r'^search(/)?$', Search.as_view(), name='search'),
-    url(r'^search/show(/)?$', SearchMedia.as_view(), name='show'),
-    url(r'^search/movie(/)?$', SearchMedia.as_view(), name='movie'),
-    url(r'^search/subtitles(/)?$', SearchSubtitles.as_view(), name='subtitles')
+    path('search', Search.as_view(), name='search'),
+    path('search/show', SearchMedia.as_view(), name='show'),
+    path('search/movie', SearchMedia.as_view(), name='movie'),
+    path('search/subtitles', SearchSubtitles.as_view(), name='subtitles')
 ]
