646dc5bccbc5e8c392fe0d95fffa14ff88464f67
Spimy <officialspyme@gmail.com>
2020-10-04 15:20:53 +0400

    Make trailing slash in api urls optional

--- apps/v1/search/urls.py
+++ apps/v1/search/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from .views import Search, SearchMedia, SearchSubtitles
 
 
 app_name = 'search_v1'
 
 urlpatterns = [
-    path('search/', Search.as_view(), name='search'),
-    path('search/show/', SearchMedia.as_view(), name='show'),
-    path('search/movie/', SearchMedia.as_view(), name='movie'),
-    path('search/subtitles/', SearchSubtitles.as_view(), name='subtitles')
+    url(r'^search(/)?$', Search.as_view(), name='search'),
+    url(r'^search/show(/)?$', SearchMedia.as_view(), name='show'),
+    url(r'^search/movie(/)?$', SearchMedia.as_view(), name='movie'),
+    url(r'^search/subtitles(/)?$', SearchSubtitles.as_view(), name='subtitles')
 ]
