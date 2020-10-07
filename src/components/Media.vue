1036c87c1d0b4a3ec5006228cc986c64cf8c396e
Eray Chumak <eray_chumak@protonmail.com>
2020-10-07 17:47:32 +0100

    Add Media component

---
+++ src/components/Media.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="media">
+    <Poster name="poster" :src="poster"/>
+    <div class="name">
+      <img
+        class="banner"
+        v-if="banner"
+        :src="banner"
+        loading="lazy"
+      />
+      <p>{{title}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Poster from './Poster.vue'
+
+export default {
+  name: 'Media',
+  components: {
+    Poster
+  },
+  props: {
+    poster: String,
+    banner: String,
+    title: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.media {
+  background: #2C343F;
+  border-radius: 5px;
+  text-align: center;
+  cursor: pointer;
+  display: grid;
+  min-height: 150px;
+  grid-template-rows: 1fr auto;
+
+  &:hover, &:focus {
+    img, .noImage {
+      filter: brightness(1);
+    }
+  }
+}
+
+.name {
+  height: 100%;
+  position: relative;
+}
+
+.banner {
+  position: absolute;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  border-radius: 5px;
+  object-fit: cover;
+  object-position: 0 80%;
+  opacity: .1;
+}
+
+p {
+  padding: 1em;
+  z-index: 1;
+}
+
+@media only screen and (min-width: 350px) {
+  .media {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: 1fr !important;
+    align-items: center;
+    text-align: left !important;
+
+    .name {
+      display: flex;
+      align-items: center;
+
+      p {
+        padding: 0 1em;
+      }
+    }
+  }
+}
+
+</style>
