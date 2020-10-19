9b82887f569816e42ff9b53af591076aa2dd018f
Spimy <officialspyme@gmail.com>
2020-10-19 20:09:24 +0400

    Add exceptions and their equivalent status code

--- apps/v1/search/exceptions.py
+++ apps/v1/search/exceptions.py
@@ -2,6 +2,26 @@ from rest_framework.views import exception_handler
 from rest_framework import status
 
 
+def get_exception(key: str) -> int:
+    '''
+    Key: ErrType
+    Value: Status Code
+    Return: Status Code
+    '''
+    EXCEPTIONS = {
+        'NO_QUERY': 400,
+        'INVALID_TYPE': 404,
+        'INVALID_REQ_METHOD': 405,
+        'NO_ID': 400,
+        'NOT_FOUND': 404,
+        'INVALID_REQ_METHOD': 405,
+        'WRONG_LANG_CODE': 400,
+        'WRONG_ID': 404,
+        'INVALID_REQ_METHOD': 405
+    }
+    return EXCEPTIONS[key]
+
+
 def handle_405_exception(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
