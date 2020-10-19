175104cb6c7c6479c4d78ef43e9a2369d1877e07
Spimy <officialspyme@gmail.com>
2020-10-19 19:44:07 +0400

    Remove movie and show endpoints

--- apps/v1/search/urls.py
+++ apps/v1/search/urls.py
@@ -6,7 +6,5 @@ app_name = 'search_v1'
 
 urlpatterns = [
     path('search', Search.as_view(), name='search'),
-    path('search/show', SearchMedia.as_view(), name='show'),
-    path('search/movie', SearchMedia.as_view(), name='movie'),
-    path('search/subtitles', SearchSubtitles.as_view(), name='subtitles')
+    path('subtitles', SearchSubtitles.as_view(), name='subtitles')
 ]
