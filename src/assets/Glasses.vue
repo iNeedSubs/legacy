e1433a8dec814710fcf1e8c2854a7c664bdd5789
Eray Chumak <eray_chumak@protonmail.com>
2020-10-21 14:41:11 +0100

    Add width to glasses bridge

--- src/assets/Glasses.vue
+++ src/assets/Glasses.vue
@@ -3,7 +3,7 @@
     <g transform="translate(-19.999 -60)">
       <path fill="#dddddd" d="M-8390-1569h-90a5.006,5.006,0,0,1-5-5v-50a5.006,5.006,0,0,1,5-5h90a5.006,5.006,0,0,1,5,5v50A5.006,5.006,0,0,1-8390-1569Zm-80-55a5.006,5.006,0,0,0-5,5v40a5.006,5.006,0,0,0,5,5h70a5.006,5.006,0,0,0,5-5v-40a5.006,5.006,0,0,0-5-5Z" transform="translate(8505 1689.001)"/>
       <path fill="#dddddd" d="M-8390-1569h-90a5.006,5.006,0,0,1-5-5v-50a5.006,5.006,0,0,1,5-5h90a5.006,5.006,0,0,1,5,5v50A5.006,5.006,0,0,1-8390-1569Zm-80-55a5.006,5.006,0,0,0-5,5v40a5.006,5.006,0,0,0,5,5h70a5.006,5.006,0,0,0,5-5v-40a5.006,5.006,0,0,0-5-5Z" transform="translate(8645 1689.001)"/>
-      <rect fill="#dddddd" width="40" height="10" transform="translate(120 85)"/>
+      <rect fill="#dddddd" width="50" height="10" transform="translate(115 85)"/>
       <rect id="glowLeft" fill="#1780e8" opacity=".4" width="80" height="50" rx="5" transform="translate(30 65)"/>
       <rect id="glowRight" fill="#eb5857" opacity=".4" width="80" height="50" rx="5" transform="translate(170 65)"/>
     </g>
