e36a986ac6d9a139e73199dfa05f2c95cc57de95
Eray Chumak <eray_chumak@protonmail.com>
2020-11-02 20:56:46 +0000

    Add ErrBubble component

---
+++ src/components/ErrBubble.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="errBubble">
+    <div class="msg">
+      <TimesCircleIcon/>
+      <p>{{msg}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TimesCircleIcon from '@/assets/icons/TimesCircle.vue'
+
+export default defineComponent({
+  name: 'ErrBubble',
+  components: {
+    TimesCircleIcon
+  },
+  props: {
+    msg: String
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.errBubble {
+  text-align: center;
+}
+
+.msg {
+  display: inline-flex;
+  align-items: center;
+  background: #D63131;
+  padding: .5em 1em;
+  border-radius: 15px;
+  text-align: left;
+
+  svg {
+    margin-right: 10px;
+  }
+}
+</style>
